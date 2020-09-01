0a7d4056b0424c37d4819c23fae1476c81cf87f4
21-Lifeforms-3 <joyfulnoises2003@yahoo.com>
2020-09-01 01:24:18 -0400

    good

--- site/js/MYscripts.js
+++ site/js/MYscripts.js
@@ -3,10 +3,12 @@
 x = 0;
 function switchImg() {
 var closet = ["../images/char_group/alien.png", "../images/char_group/witch.png", "../images/char_group/devil.png", "../images/char_group/bear.png", "../images/char_group/bee.png", "../images/char_group/convict.png", "../images/char_group/elf.png", "../images/char_group/frankmonster.png", "../images/char_group/girl-pirate.png", "../images/char_group/girl-pumpkin.png", "../images/char_group/gray-vamp.png", "../images/char_group/green-witch.png", "../images/char_group/mermaid.png", "../images/char_group/mummy.png", "../images/char_group/pirate.png", "../images/char_group/pumpkin.png", "../images/char_group/ridinghood.png", "../images/char_group/vamp.png"];
-
  var image = document.getElementById("imgToSwitch");
  var choicex = closet[x];
  image.src = choicex;
+ var choicex = choicex.substring(0, choicex.length -4);
+ var choicex = choicex.concat("-clear.png");
+ userData.choicex = choicex; 
  x++;
  if (x > 17) {x = 0};
 };
@@ -14,10 +16,10 @@ var closet = ["../images/char_group/alien.png", "../images/char_group/witch.png"
 y = 0;
 function switchhome() {
 var closet = ["../images/home_group/2.png", "../images/home_group/3.png", "../images/home_group/4.png", "../images/home_group/5.png", "../images/home_group/6.png", "../images/home_group/7.png", "../images/home_group/8.png", "../images/home_group/9.png", "../images/home_group/10.png", "../images/home_group/11.png", "../images/home_group/12.png", "../images/home_group/13.png"];
-
  var image = document.getElementById("homeToSwitch");
  var choicey = closet[y];
  image.src = choicey;
+ userData.choicey = choicey;
  y++;
  if (y > 11) {y = 0};
 };
@@ -27,6 +29,7 @@ function swFright() {
   var level = ["../images/intensity-low.png", "../images/intensity-med.png", "../images/intensity-high.png"];
   var image = document.getElementById("intensity")
   var lvl = level[z];
+  userData.lvl = z;
   image.src = lvl;
   z++;
   if (z > 2) {z = 0};
@@ -40,6 +43,7 @@ function newname() {
  var ni2 = middlenames[Math.floor(Math.random() * middlenames.length)];
  var ni3 = lastnames[Math.floor(Math.random() * lastnames.length)];
 newn = ni1.concat(ni2, ni3);
+userData.newn = newn;
 document.getElementById("newn").innerText = newn
 };
     //    CHNAGE TITLE   //
@@ -51,21 +55,39 @@ function newtitle() {
   var ti2 = middlenames[Math.floor(Math.random() * middlenames.length)];
   var ti3 = lastnames[Math.floor(Math.random() * lastnames.length)];
 newt = ti1.concat(ti2, ti3);
+userData.newt = newt;
 document.getElementById("newt").innerText = newt
 };
      //  STORE USER DATA   //
+    var userData = [];
+    var GameData = [];
 function storeUser() {
 //   pwd, email, choicex, choicey, lvl, newn, newt, ...
-  var userData = [];
-  userData[0] = getElementById(email).innerText = email;
-  userData[1] = getElementById(choicex).innerText = choicex;
-  userData[2] = getElementById(choicey).innerText = choicey;
-  userData[3] = getElementById(lvl).innerText = lvl;
-  userData[4] = getElementById(newn).innerText = newn;
-  userData[5] = getElementById(newt).innerText = newt;
-  Pname = getElementById(pwd).innerText = pwd;
-  myJSON = JSON.stringify(userData);
-  localStorage.setItem("Pname", myJSON);
+
+  if (userData.choicex === undefined) {
+    x = Math.floor(Math.random() * 16);
+    switchImg();
+  };
+    if (userData.choicey === undefined) {
+    x = Math.floor(Math.random() * 10);
+    switchhome();
+  };
+    if (userData.newn === undefined) {
+    newname();
+  };
+    if (userData.newt === undefined) {
+    newtitle();
+  };
+    userData.pwd = document.getElementById("joining").elements[1].value;
+    userData.email = document.getElementById("joining").elements[0].value;
+
+
+ GameData[GameData.length] = userData;
+ myJSON = JSON.stringify(GameData);
+ localStorage.setItem("GameData", myJSON);
+  console.log(userData);
+  console.log(GameData);
+   
 };
 function readUser() {
   Pname = getElementById(pwd).innerText = pwd;
